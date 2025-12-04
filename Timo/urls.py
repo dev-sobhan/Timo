5efacddc2506e647a6834e7a5b36d92e9371e473
sobhan nami <namisobhanss@gmail.com>
2025-12-04 13:15:33 +0330

    add schema urls paths

--- Timo/urls.py
+++ Timo/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path('api/users/', include('users.urls', namespace='users')),
     path('api/teams/', include('teams.urls', namespace='teams')),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Swagger UI
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+    # ReDoc UI
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc-ui'),
+
 ]
